c44c68c835fa2617424a3a7da331ad662d71c630
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-30 18:26:51 +0100

    Remove unneeded role name

--- src/main/java/pt/ua/deti/tqs/backend/constants/UserRole.java
+++ src/main/java/pt/ua/deti/tqs/backend/constants/UserRole.java
@@ -1,14 +1,7 @@
 package pt.ua.deti.tqs.backend.constants;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public enum UserRole {
-    USER("ROLE_USER"),
-    STAFF("ROLE_STAFF"),
-    ADMIN("ROLE_ADMIN");
-
-    private final String roleName;
+    USER,
+    STAFF,
+    ADMIN;
 }
