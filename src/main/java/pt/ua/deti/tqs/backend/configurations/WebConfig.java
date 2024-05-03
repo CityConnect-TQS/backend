2bb1855f095ff41ccec69f8e53c458f02a738662
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-05-03 15:38:00 +0100

    Move CORS bean to a single configuration file

--- src/main/java/pt/ua/deti/tqs/backend/configurations/WebConfig.java
+++ src/main/java/pt/ua/deti/tqs/backend/configurations/WebConfig.java
@@ -1,9 +1,16 @@
 package pt.ua.deti.tqs.backend.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -14,5 +21,16 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOriginPatterns("http://*.localhost", "http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("http://*.localhost", "http://localhost"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
