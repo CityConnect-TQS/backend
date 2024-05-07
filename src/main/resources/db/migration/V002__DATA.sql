146b44690f647bd15bb7282c46f452b0c003a3ae
Bárbara Galiza <barnobregali02@gmail.com>
2024-05-07 22:56:35 +0100

    hotfix of missing lat and lon in sql file

--- src/main/resources/db/migration/V002__DATA.sql
+++ src/main/resources/db/migration/V002__DATA.sql
@@ -7,7 +7,7 @@ VALUES (1, 50, 'Flix Bus'),
        (5, 20, 'Flix Bus');
        
 
-INSERT INTO public.city (id, name)
+INSERT INTO public.city (id, name, latitude, longitude)
 VALUES (1, 'Murtosa', 40.7608, -8.6375),
        (2, 'Estarreja', 40.7583, -8.5725),
        (3, 'Aveiro', 40.6401, -8.6538),
