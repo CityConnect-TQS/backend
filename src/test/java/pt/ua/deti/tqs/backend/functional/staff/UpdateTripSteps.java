8ff10b0cc783706542c744df38b63d7d99712655
Diana <dianarrmiranda@gmail.com>
2024-05-19 23:20:36 +0100

    CC-320 frontend tests

---
+++ src/test/java/pt/ua/deti/tqs/backend/functional/staff/UpdateTripSteps.java
@@ -0,0 +1,54 @@
+package pt.ua.deti.tqs.backend.functional.staff;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class UpdateTripSteps {
+
+    private final WebDriver driver = StaffCucumberTest.getDriver();
+
+    @And("I click on the edit button of the trip {int} witch is from {string} to {string}")
+    public void iClickOnTheEditButtonOfTheTripWitchIsFromTo(int tripId, String origin, String destination) {
+        assertThat(driver.findElement(By.id("departureName" +tripId)).getText()).isEqualTo(origin);
+        assertThat(driver.findElement(By.id("arrivalName" +tripId)).getText()).isEqualTo(destination);
+        driver.findElement(By.id("editTrip" + tripId)).click();
+    }
+    @And("I change the price from {double} to {double}")
+    public void iChangeThePriceFromTo(double oldPrice, double newPrice) {
+        assertThat(Double.parseDouble(driver.findElement(By.id("priceInput")).getAttribute("value"))).isEqualTo(oldPrice);
+        driver.findElement(By.id("priceInput")).clear();
+        driver.findElement(By.id("priceInput")).sendKeys(String.valueOf(newPrice));
+    }
+
+    @And("I click on the save button")
+    public void iClickOnTheSaveButton() {
+        driver.findElement(By.id("saveTripBtn")).click();
+    }
+
+    @And("I should see a edit button")
+    public void iShouldSeeAEditButton() {
+        assertThat(driver.findElement(By.id("editTripBtn")).isDisplayed()).isTrue();
+    }
+
+    @And("I change the arrival city to {string}")
+    public void iChangeTheArrivalCityTo(String arrivalCity) {
+        driver.findElement(By.id("arrivalCity")).click();
+        driver.findElement(By.id("arrival" + arrivalCity)).click();
+    }
+
+    @Then("I should see {double} as the price of the trip")
+    public void iShouldSeeAsThePriceOfTheTrip(double price) {
+        assertThat(Double.parseDouble(driver.findElement(By.id("priceInput")).getAttribute("value"))).isEqualTo(price);
+    }
+
+    @Then("I should see a error message saying {string}")
+    public void iShouldSeeAErrorMessageSaying(String message) {
+        assertThat(driver.findElement(By.id("errorMessageSpan")).getText()).isEqualTo(message);
+    }
+
+
+}
--- src/test/resources/pt/ua/deti/tqs/backend/functional/staff/deleteTrip.feature
+++ src/test/resources/pt/ua/deti/tqs/backend/functional/staff/deleteTrip.feature
@@ -13,8 +13,8 @@ Feature: Delete trip
 
   Scenario: Delete a trip
     And I should see a list of trips
-    And I click on the delete button of the trip 16 witch is from "Aveiro" to "Murtosa"
-    And I should see a modal with the title "Delete Trip Aveiro - Murtosa"
+    And I click on the delete button of the trip 20 witch is from "Aveiro" to "Ovar"
+    And I should see a modal with the title "Delete Trip Aveiro - Ovar"
     And I click on the delete button
     Then I should see a success message with the text "Trip deleted successfully"
 
