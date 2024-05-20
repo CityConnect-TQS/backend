ab01a5e3281cd43907cdfbcf9f1337bf553a832b
Bárbara Galiza <barnobregali02@gmail.com>
2024-05-20 18:46:54 +0100

    first scenario passes

--- src/main/resources/db/migration/V002__DATA.sql
+++ src/main/resources/db/migration/V002__DATA.sql
@@ -17,7 +17,8 @@ INSERT INTO "user" (id, name, email, password)
 VALUES ('1', 'admin', 'admin@gmail.com', '$2a$10$k8mC7DODg5iNmrp3B7V.R.jupGeT.gA7cFYqzcAo/w9bYVFZ7F4ty');
 
 INSERT INTO role (user_id, roles)
-VALUES (1, 2);
+VALUES (1, 2),
+       (1, 0);
 
 INSERT INTO public.trip (id, arrival_time, departure_time, free_seats, price, arrival_id, bus_id, departure_id)
 VALUES (1, '2024-05-08 08:00:00', '2024-05-08 06:00:00', 36, 10.00, 3, 1, 1),
--- src/test/java/pt/ua/deti/tqs/backend/functional/client/BookReservationSteps.java
+++ src/test/java/pt/ua/deti/tqs/backend/functional/client/BookReservationSteps.java
@@ -1,36 +1,79 @@
 package pt.ua.deti.tqs.backend.functional.client;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.By;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Given;
-import org.openqa.selenium.By;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookReservationSteps {
     
-    private WebDriver driver;
+    private final WebDriver driver = ClientCucumberTest.getDriver();
+    private Wait<WebDriver> wait;
 
     @Given("I am at the url {string}")
-    public void iNavigateTo(String url) {
-        FirefoxOptions options = new FirefoxOptions();
-        options.addArguments("-headless");
-
-        driver = new FirefoxDriver(options);
+    public void iAmAt(String url) {
         driver.get(url);
     }
 
+    @Given("I am signed in")
+    public void iAmSignedIn() throws InterruptedException{
+        wait = new WebDriverWait(driver, Duration.of(10, ChronoUnit.SECONDS));
+        wait.until(d -> d.findElement(By.id("avatarBtn")).isDisplayed());
+        TimeUnit.SECONDS.sleep(1);
+        driver.findElement(By.id("avatarBtn")).click();
+        driver.findElement(By.id("loginBtn")).click();
+        driver.findElement(By.id("email")).sendKeys("admin@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("admin123");
+        driver.findElement(By.id("loginBtn")).click();
+    }
+
     @When("I click on the first trip that shows up")
-    public void iClickOnTheFirstTrip() {
-        driver.findElement(By.cssSelector("#trips>div:nth-child(1)")).click();
+    public void iClickOnTheFirstTrip() throws InterruptedException{
+    	TimeUnit.SECONDS.sleep(3);
+        driver.findElement(By.id("tripCard41")).click();
     }
 
     @Then("I should be redirected to {string} page")
-    public void iShouldBeRedirectedTo(String title) {
+    public void iShouldBeRedirectedTo(String title) throws InterruptedException{
+    	TimeUnit.SECONDS.sleep(1);
         assertThat(driver.findElement(By.id("pageTitle")).getText()).isEqualTo(title);
+    }
+
+    @Then("the submit button should be disabled")
+    public void theSubmitButtonShouldBeDisabled() {
+        assertThat(driver.findElement(By.id("submitBtn")).isEnabled()).isFalse();
+    }
+
+    @When("I click on seat {string} and seat {string}")
+    public void iClickOnSeat(String seat1, String seat2) throws InterruptedException{
+        driver.findElement(By.id("seat" + seat1)).click();
+        driver.findElement(By.id("seat" + seat2)).click();
+    }
+
+    @When("I click on submit button")
+    public void iClickOnSubmitButton() throws InterruptedException{
+        TimeUnit.SECONDS.sleep(1);
+        driver.findElement(By.id("submitBtn")).click();
+    }
+
+    @Then("I should see the message {string}")
+    public void iShouldSeeTheMessage(String message) throws InterruptedException{
+        TimeUnit.SECONDS.sleep(1);
+        assertThat(driver.findElement(By.id("confirmText")).getText()).isEqualTo(message);
+    }
+    
+    @Then("the seats {string} and {string} should appear")
+    public void theSeatsShouldAppear(String seat1, String seat2) {
+    	assertThat(driver.findElement(By.id("selectedSeats")).getText()).isEqualTo(seat1 + ", " + seat2);
     }
 }
